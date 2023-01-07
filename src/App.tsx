ee81aa14f0a6682bd0c433c0d24b9f0a12ee43db
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2023-01-07 21:10:09 +0900

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,119 +4,50 @@ import Dots from "./dot";
 
 function App() {
   const outerRef = useRef<any>();
+  const pageHeight = window.innerHeight;
+  const [scrollIndex, setScrollIndex] = useState(1);
+  
+  const removeEvent = () => {
+    const outerDivRefCurrent = outerRef.current;
+
+    outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+
+    setTimeout(
+      () => {
+        outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+      },1000);
+  };
+
+  const outerToScroll = (height: number, DIVIDER: number, index: number) => {
+    outerRef.current.scrollTo({
+      top: height + DIVIDER,
+      left: 0,
+      behavior: "smooth",
+    });
+    setScrollIndex(index);
+  };
 
   const wheelHandler = (e: any) => {
     e.preventDefault();
     const { deltaY } = e;
     const { scrollTop } = outerRef.current;
-    const pageHeight = window.innerHeight;
-    const DiVIDER_HEIGHT = 5;
+    const DIVIDER_HEIGHT = 5;
 
     if (deltaY > 0) {
-      if (scrollTop >= 0 && scrollTop < pageHeight && state) {
-        if (state.current) {
-          outerRef.current.scrollTo({
-            top: pageHeight + DiVIDER_HEIGHT,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(2);
-        }
-        state.current = false;
-        setTimeout(() => {
-          if (!state.current) {
-            state.current = true;
-          }
-        }, 2000);
-      } else if (
-        scrollTop >= pageHeight &&
-        scrollTop < pageHeight * 2 &&
-        state
-      ) {
-        if (state.current) {
-          outerRef.current.scrollTo({
-            top: pageHeight * 2 + DiVIDER_HEIGHT * 2,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(3);
-        }
-        state.current = false;
-        setTimeout(() => {
-          if (!state.current) {
-            state.current = true;
-          }
-        }, 2000);
-      } else if (
-        scrollTop >= pageHeight &&
-        scrollTop < pageHeight * 3 &&
-        state
-      ) {
-        if (state.current) {
-          outerRef.current.scrollTo({
-            top: pageHeight * 2 + DiVIDER_HEIGHT * 2,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(3);
-        }
-        state.current = false;
-        setTimeout(() => {
-          if (!state.current) {
-            state.current = true;
-          }
-        }, 2000);
+      if (scrollTop >= 0 && scrollTop < pageHeight) {
+        outerToScroll(pageHeight, DIVIDER_HEIGHT, 2);
+      } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+        outerToScroll(pageHeight * 2, DIVIDER_HEIGHT * 2, 3);
       }
     } else {
-      if (scrollTop >= 0 && scrollTop < pageHeight && state) {
-        if (state.current) {
-          outerRef.current.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(1);
-        }
-        state.current = false;
-        setTimeout(() => {
-          if (!state.current) {
-            state.current = true;
-          }
-        }, 2000);
-      } else if (scrollTop >= 0 && scrollTop < pageHeight * 2 && state) {
-        if (state.current) {
-          outerRef.current.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(1);
-        }
-        state.current = false;
-        setTimeout(() => {
-          if (!state.current) {
-            state.current = true;
-          }
-        }, 2000);
-      } else if (scrollTop >= 0 && scrollTop < pageHeight * 3 && state) {
-        if (state.current) {
-          console.log(state.current);
-          
-          outerRef.current.scrollTo({
-            top: pageHeight + DiVIDER_HEIGHT,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(2);
-        }
-        state.current = false;
-        setTimeout(() => {
-          if (!state.current) {
-            state.current = true;
-          }
-        }, 2000);
+      if (scrollTop >= 0 && scrollTop < pageHeight * 2) {
+        outerToScroll(0, 0, 1);
+      } else if (scrollTop >= 0 && scrollTop < pageHeight * 3) {
+        outerToScroll(pageHeight, DIVIDER_HEIGHT, 2);
       }
     }
+
+    removeEvent();
   };
 
   useEffect(() => {
@@ -129,9 +60,6 @@ function App() {
       outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
     };
   }, []);
-
-  const [scrollIndex, setScrollIndex] = useState(1);
-  const state = useRef(true);
 
   return (
     <div ref={outerRef} className="outer">
